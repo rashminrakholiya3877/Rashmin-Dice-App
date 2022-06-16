8611163eeb3fe5c76f299a0bf06c38f29c27f421
Rashmin <rashmin3877@gmail.com>
2022-06-16 14:26:13 +0530

    Initial Commit

---
+++ Dice App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dice App
+//
+//  Created by RASHIMN on 16/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
